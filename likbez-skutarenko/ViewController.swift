2299ca81f05218727d2fd08d7676f403e098ed5b
Arthur <arthur@Arthurs-MacBook-Air.local>
2018-11-24 13:43:21 +0300

    Initial Commit

---
+++ likbez-skutarenko/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  likbez-skutarenko
+//
+//  Created by Arthur on 24/11/2018.
+//  Copyright © 2018 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
